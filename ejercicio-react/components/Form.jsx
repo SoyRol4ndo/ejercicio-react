def20473d298b8a616e8bf7d7a221efc5a622c7a
Rol4ndo <rol4ndo99@gmail.com>
2023-06-16 13:51:51 +0200

    Reading and cleaning the Form

--- ejercicio-react/components/Form.jsx
+++ ejercicio-react/components/Form.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
 
+  // State de usuario
+  const [user, setuser] = useState('');
+
+  // Cuando el usuario da submit al form
+  const onSubmitUser = e => {
+    e.preventDefault();
+
+    // Validar que no este vacio el input
+    if (user.trim() === '') {
+      console.log('error');
+      return;
+    }
+    // Pasar el usuario al state general
+
+
+    // Mandar a consultar la API
+
+
+    // Reiniciar el form
+    setuser('');
+  };
+
   return (
-    <form>
+    <form
+      onSubmit={onSubmitUser}
+    >
       <label htmlFor="user" >User: </label>
 
       <input
         type="text"
         placeholder='User GitHub'
         name='user'
+        value={user}
+        onChange={e => setuser(e.target.value)}
       />
 
-      <input
-        value='Buscar'
-        type="submit"
-      />
+      <input type="submit" />
     </form>
 
   );
